a19b82e1f19961557dfe4c440459a864820c433d
kinghatrung <nguyenred2003@gmail.com>
2025-10-16 23:29:08 +0700

    feat: created button facebook message

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -34,6 +34,7 @@ import Products from '~/pages/PublicPages/Products'
 import Services from '~/pages/PublicPages/Services'
 import Contact from '~/pages/PublicPages/Contact'
 import Product from '~/pages/PublicPages/Products/[id]'
+import ButtonFacebook from './components/common/ButtonFacebook'
 
 const ProtectedRoute = ({ user }) => {
   if (!user) return <Navigate to={ROUTES.LOGIN} replace />
@@ -56,7 +57,16 @@ function App() {
     <div className='App'>
       <ScrollToTop />
       <Toaster richColors position='bottom-right' />
-      <ButtonZalo phone='0961753837' size={60} position={{ bottom: 30, right: 30 }} />
+      <ButtonZalo
+        phone={import.meta.env.VITE_ZALO_PHONE}
+        size={60}
+        position={{ bottom: 30, right: 30 }}
+      />
+      <ButtonFacebook
+        pageId={import.meta.env.VITE_FACEBOOK_URL}
+        size={60}
+        position={{ bottom: 110, right: 30 }}
+      />
 
       <Routes>
         {/* Public */}
